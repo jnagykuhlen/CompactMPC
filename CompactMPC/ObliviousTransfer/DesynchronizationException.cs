94af3077e3007be105be88d17ed2ef339f5abd18
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2019-06-09 10:54:26 +0200

    Moved DesynchronizationException to ObliviousTransfer namespace.

--- CompactMPC/ObliviousTransfer/DesynchronizationException.cs
+++ CompactMPC/ObliviousTransfer/DesynchronizationException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CompactMPC
+namespace CompactMPC.ObliviousTransfer
 {
     public class DesynchronizationException : Exception
     {
