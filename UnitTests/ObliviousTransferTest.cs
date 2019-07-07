fcee175b35d7736bbd5b086f46f487134a82c190
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2019-07-07 13:19:40 +0300

    Added test for 1oo6 OT (to show that implementation is indeed general 1ooN)

--- UnitTests/ObliviousTransferTest.cs
+++ UnitTests/ObliviousTransferTest.cs
@@ -15,10 +15,23 @@ namespace CompactMPC.UnitTests
     [TestClass]
     public class ObliviousTransferTest
     {
-        private static readonly string[] TestOptions = { "Alicia", "Briann", "Charly", "Dennis" };
+        // todo(lumip): these tests are mostly copy+paste right now. should be streamlined more. also, it would be
+        //  sufficient to test that the wrapper functions of GeneralizedObliviousTransfer (1oo2, 1oo4) forward correctly
+        //  and have only one test for the real 1ooN OT implementation
+
+        private static readonly string[] TestOptions = { "Alicia", "Briann", "Charly", "Dennis", "Elenor", "Frieda" };
 
         [TestMethod]
         public void TestNaorPinkasObliviousTransfer1oo4()
+        {
+            Task.WhenAll(
+                Task.Factory.StartNew(RunObliviousTransferParty1oo4, TaskCreationOptions.LongRunning),
+                Task.Factory.StartNew(RunObliviousTransferParty1oo4, TaskCreationOptions.LongRunning)
+            ).Wait();
+        }
+
+        [TestMethod]
+        public void TestNaorPinkasObliviousTransfer1oo6()
         {
             Task.WhenAll(
                 Task.Factory.StartNew(RunObliviousTransferParty1oo4, TaskCreationOptions.LongRunning),
@@ -55,6 +68,54 @@ namespace CompactMPC.UnitTests
                     {
                         QuadrupleIndexArray indices = new QuadrupleIndexArray(new[] { 0, 3, 2 });
                         byte[][] results = obliviousTransfer.ReceiveAsync(session.Channel, indices, numberOfInvocations, 6).Result;
+
+                        Assert.IsNotNull(results, "Result is null.");
+                        Assert.AreEqual(numberOfInvocations, results.Length, "Result does not match the correct number of invocations.");
+
+                        for (int j = 0; j < numberOfInvocations; ++j)
+                        {
+                            CollectionAssert.AreEqual(
+                                results[j],
+                                options[j][indices[j]],
+                                "Incorrect message content {0} (should be {1}).",
+                                Encoding.ASCII.GetString(results[j]),
+                                Encoding.ASCII.GetString(options[j][indices[j]])
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RunObliviousTransferParty1oo6()
+        {
+            const int numberOfInvocations = 4;
+            const int numberOfOptions = 6;
+            byte[][][] options = new byte[][][]
+            {
+                TestOptions.Take(numberOfOptions).Select(s => Encoding.ASCII.GetBytes(s)).ToArray(),
+                TestOptions.Take(numberOfOptions).Select(s => Encoding.ASCII.GetBytes(s.ToLower())).ToArray(),
+                TestOptions.Take(numberOfOptions).Select(s => Encoding.ASCII.GetBytes(s.ToUpper())).ToArray(),
+                TestOptions.Take(numberOfOptions).Select(s => Encoding.ASCII.GetBytes(s.ToUpper())).ToArray(),
+            };
+
+            using (CryptoContext cryptoContext = CryptoContext.CreateDefault())
+            {
+                IGeneralizedObliviousTransfer obliviousTransfer = new NaorPinkasObliviousTransfer(
+                    SecurityParameters.CreateDefault768Bit(),
+                    cryptoContext
+                );
+
+                using (ITwoPartyNetworkSession session = TestNetworkSession.EstablishTwoParty())
+                {
+                    if (session.LocalParty.Id == 0)
+                    {
+                        obliviousTransfer.SendAsync(session.Channel, options, numberOfOptions, numberOfInvocations, 6).Wait();
+                    }
+                    else
+                    {
+                        int[] indices = new[] { 4, 1, 3, 5 };
+                        byte[][] results = obliviousTransfer.ReceiveAsync(session.Channel, indices, numberOfOptions, numberOfInvocations, 6).Result;
 
                         Assert.IsNotNull(results, "Result is null.");
                         Assert.AreEqual(numberOfInvocations, results.Length, "Result does not match the correct number of invocations.");
