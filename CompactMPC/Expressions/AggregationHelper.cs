d22c389131979989424d7685d890a92542d14e81
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2020-05-09 17:06:13 +0200

    Renamed ExpressionHelper to AggregationHelper. Added unit tests.

---
+++ CompactMPC/Expressions/AggregationHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompactMPC.Expressions
+{
+    public static class AggregationHelper
+    {
+        public static T AggregateDepthEfficient<T>(this IReadOnlyCollection<T> source, Func<T, T, T> func)
+        {
+            return AggregateDepthEfficient(source, func, source.Count);
+        }
+
+        private static T AggregateDepthEfficient<T>(IEnumerable<T> source, Func<T, T, T> func, int count)
+        {
+            if (count == 0)
+                throw new ArgumentException("Aggregation requires at least one operand.", nameof(source));
+
+            if (count == 1)
+                return source.First();
+            
+            return AggregateDepthEfficient(Reduce(source, func), func, (count + 1) / 2);
+        }
+
+        private static IEnumerable<T> Reduce<T>(IEnumerable<T> source, Func<T, T, T> func)
+        {
+            T previous = default!;
+            bool parity = false;
+
+            foreach (T current in source)
+            {
+                if (parity)
+                {
+                    yield return func(previous, current);
+                }
+
+                previous = current;
+                parity = !parity;
+            }
+
+            if (parity)
+                yield return previous;
+        }
+    }
+}
---
+++ UnitTests/Expressions/AggregationHelperTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CompactMPC.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompactMPC.UnitTests.Expressions
+{
+    [TestClass]
+    public class AggregationHelperTest
+    {
+        [TestMethod]
+        public void TestAggregateDepthEfficientAggregatesSingleValue()
+        {
+            int result = new[] { 3 }.AggregateDepthEfficient((x, y) => x + y);
+
+            Assert.AreEqual(3, result);
+        }
+        
+        [TestMethod]
+        public void TestAggregateDepthEfficientAggregatesEvenNumberOfValues()
+        {
+            int result = new[] { 1, 3, 5, 4 }.AggregateDepthEfficient((x, y) => x + y);
+            
+            Assert.AreEqual(13, result);
+        }
+        
+        [TestMethod]
+        public void TestAggregateDepthEfficientAggregatesOddNumberOfValues()
+        {
+            int result = new[] { 8, 3, 5, 4, 1 }.AggregateDepthEfficient((x, y) => x + y);
+            
+            Assert.AreEqual(21, result);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAggregateDepthEfficientDoesNotAggregateZeroValues()
+        {
+            new int[] { }.AggregateDepthEfficient((x, y) => x + y);
+        }
+        
+        [TestMethod]
+        public void TestAggregateDepthEfficientAggregatesWithMinimumDepth()
+        {
+            int depth = new[] { 0, 0, 0, 0, 0, 0, 0 }.AggregateDepthEfficient((x, y) => Math.Max(x, y) + 1);
+
+            Assert.AreEqual(3, depth);
+        }
+    }
+}
