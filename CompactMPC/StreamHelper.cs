f85b02d5a759e4c41805a7a6ce78da860eca6ba5
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2020-05-10 16:25:23 +0200

    Removed unused methods from StreamHelper.

--- CompactMPC/StreamHelper.cs
+++ CompactMPC/StreamHelper.cs
@@ -5,20 +5,6 @@ namespace CompactMPC
 {
     public static class StreamHelper
     {
-        public static byte[] Read(this Stream stream, int count)
-        {
-            byte[] buffer = new byte[count];
-            int offset = 0;
-
-            while (offset < count)
-            {
-                int readBytes = stream.Read(buffer, offset, count - offset);
-                offset += readBytes;
-            }
-
-            return buffer;
-        }
-
         public static async Task<byte[]> ReadAsync(this Stream stream, int count)
         {
             byte[] buffer = new byte[count];
@@ -32,30 +18,10 @@ namespace CompactMPC
 
             return buffer;
         }
-
-        public static async Task<int> ReadByteAsync(this Stream stream)
-        {
-            byte[] buffer = new byte[1];
-            int readBytes = await stream.ReadAsync(buffer, 0, 1);
-            if (readBytes > 0)
-                return buffer[0];
-
-            return -1;
-        }
         
-        public static void Write(this Stream stream, byte[] buffer)
-        {
-            stream.Write(buffer, 0, buffer.Length);
-        }
-
         public static Task WriteAsync(this Stream stream, byte[] buffer)
         {
             return stream.WriteAsync(buffer, 0, buffer.Length);
-        }
-
-        public static Task WriteByteAsync(this Stream stream, byte value)
-        {
-            return stream.WriteAsync(new[] { value }, 0, 1);
         }
     }
 }
