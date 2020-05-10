c9218d219f1dce7bf7b872c80eebff113b370213
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2020-05-10 13:28:51 +0200

    Implemented unit test for EnumerableHelper.

---
+++ UnitTests/EnumerableHelperTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CompactMPC.UnitTests.Assertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompactMPC.UnitTests
+{
+    [TestClass]
+    public class EnumerableHelperTest
+    {
+        [TestMethod]
+        public void TestFlatten()
+        {
+            IEnumerable<IEnumerable<int>> source = new[]
+            {
+                new[] { 1, 3, 7},
+                new[] { 2, 4 }
+            };
+
+            IEnumerable<int> flattened = source.Flatten();
+            
+            EnumerableAssert.AreEqual(new[] { 1, 3, 7, 2, 4 }, flattened);
+        }
+    }
+}
