3a264c58dc441c522993b7e0a7f35bda962b1618
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2020-07-05 17:16:17 +0200

    Split NetworkTest into multiple test classes and moved them to the networking namespace.

---
+++ CompactMPC.Tests/Networking/TcpTwoPartyNetworkSessionTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompactMPC.Networking
+{
+    [TestClass]
+    public class TcpTwoPartyNetworkSessionTest
+    {
+        private const int Port = 12674;
+        
+        private static readonly Party FirstParty = new Party(0, "First");
+        private static readonly Party SecondParty = new Party(1, "Second");
+
+        [TestMethod]
+        public void TestTcpTwoPartyNetworkSession()
+        {
+            Task<TcpTwoPartyNetworkSession> firstSessionTask = CreateFirstTwoPartySessionAsync();
+            Task<TcpTwoPartyNetworkSession> secondSessionTask = CreateSecondTwoPartySessionAsync();
+
+            using TcpTwoPartyNetworkSession firstSession = firstSessionTask.Result;
+            using TcpTwoPartyNetworkSession secondSession = secondSessionTask.Result;
+
+            firstSession.LocalParty.Should().Be(FirstParty);
+            firstSession.RemoteParty.Should().Be(SecondParty);
+            
+            secondSession.LocalParty.Should().Be(SecondParty);
+            secondSession.RemoteParty.Should().Be(FirstParty);
+        }
+
+        private static Task<TcpTwoPartyNetworkSession> CreateFirstTwoPartySessionAsync()
+        {
+            return TcpTwoPartyNetworkSession.ConnectLoopbackAsync(FirstParty, Port);
+        }
+
+        private static Task<TcpTwoPartyNetworkSession> CreateSecondTwoPartySessionAsync()
+        {
+            return TcpTwoPartyNetworkSession.AcceptLoopbackAsync(SecondParty, Port);
+        }
+    }
+}
