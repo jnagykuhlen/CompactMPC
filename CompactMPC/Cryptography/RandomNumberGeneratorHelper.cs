b1b6803f6850d82d37fd905b83b0fbb95861e238
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2022-11-05 14:05:52 +0100

    Make BigInteger returned from RandomNumberGeneratorHelper unsigned and simplify logic

--- CompactMPC/Cryptography/RandomNumberGeneratorHelper.cs
+++ CompactMPC/Cryptography/RandomNumberGeneratorHelper.cs
@@ -7,25 +7,21 @@ namespace CompactMPC.Cryptography
     {
         public static byte[] GetBytes(this RandomNumberGenerator randomNumberGenerator, int numberOfBytes)
         {
-            byte[] randomSource = new byte[numberOfBytes];
-            randomNumberGenerator.GetBytes(randomSource);
-            return randomSource;
+            byte[] randomBytes = new byte[numberOfBytes];
+            randomNumberGenerator.GetBytes(randomBytes);
+            return randomBytes;
         }
 
         public static BitArray GetBits(this RandomNumberGenerator randomNumberGenerator, int numberOfBits)
         {
-            byte[] randomSource = new byte[BitArray.RequiredBytes(numberOfBits)];
-            randomNumberGenerator.GetBytes(randomSource);
-            return BitArray.FromBytes(randomSource, numberOfBits);
+            byte[] randomBytes = randomNumberGenerator.GetBytes(BitArray.RequiredBytes(numberOfBits));
+            return BitArray.FromBytes(randomBytes, numberOfBits);
         }
 
         public static BigInteger GetBigInteger(this RandomNumberGenerator randomNumberGenerator, int sizeInBytes)
         {
-            byte[] randomSource = new byte[sizeInBytes];
-            randomNumberGenerator.GetBytes(randomSource);
-            randomSource[sizeInBytes - 1] &= 0x7f;
-
-            return new BigInteger(randomSource);
+            byte[] randomBytes = randomNumberGenerator.GetBytes(sizeInBytes);
+            return new BigInteger(randomBytes, true);
         }
     }
 }
