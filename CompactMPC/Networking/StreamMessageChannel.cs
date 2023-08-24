5e35312649c82f7e9d920d43c4fcb3ce6b99faad
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2023-08-24 18:47:38 +0200

    Add stream helper extension methods

--- CompactMPC/Networking/StreamMessageChannel.cs
+++ CompactMPC/Networking/StreamMessageChannel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using CompactMPC.Buffers;
 
@@ -16,13 +15,13 @@ namespace CompactMPC.Networking
 
         public async Task<Message> ReadMessageAsync()
         {
-            int numberOfBytes = BitConverter.ToInt32(await _stream.ReadAsync(4), 0);
+            int numberOfBytes = await _stream.ReadInt32Async();
             return new Message(await _stream.ReadAsync(numberOfBytes));
         }
 
         public async Task WriteMessageAsync(Message message)
         {
-            await _stream.WriteAsync(BitConverter.GetBytes(message.Length));
+            await _stream.WriteInt32Async(message.Length);
             await _stream.WriteAsync(message.ToBuffer());
         }
     }
