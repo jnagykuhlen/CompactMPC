eb4f989e5260832cb0b1e4f6d0f49fcf4ba18788
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2024-05-05 19:54:06 +0200

    Implement Party as record

--- CompactMPC/Networking/Party.cs
+++ CompactMPC/Networking/Party.cs
@@ -2,50 +2,12 @@
 
 namespace CompactMPC.Networking
 {
-    public class Party
+    public record Party(int Id, string Name, Guid Guid)
     {
-        public int Id { get; }
-        public string Name { get; }
-        public Guid Guid { get; }
-
         public Party(int id) : this(id, $"Party {id + 1}", Guid.NewGuid())
         { }
-
-        public Party(int id, string name, Guid guid)
-        {
-            Id = id;
-            Name = name;
-            Guid = guid;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name} (id: {Id})";
-        }
-
-        public override bool Equals(object? other)
-        {
-            if (other is Party otherParty)
-                return Id == otherParty.Id && Name == otherParty.Name && Guid == otherParty.Guid;
-
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            int hashCode = 321773176;
-            hashCode = hashCode * -1521134295 + Id.GetHashCode();
-            hashCode = hashCode * -1521134295 + Name.GetHashCode();
-            hashCode = hashCode * -1521134295 + Guid.GetHashCode();
-            return hashCode;
-        }
-
-        public static bool operator>(Party first, Party second) {
-            return first.Guid.CompareTo(second.Guid) > 0;
-        }
         
-        public static bool operator<(Party first, Party second) {
-            return second > first;
-        }
+        public static bool operator>(Party first, Party second) => first.Guid.CompareTo(second.Guid) > 0;
+        public static bool operator<(Party first, Party second) => second > first;
     }
 }
