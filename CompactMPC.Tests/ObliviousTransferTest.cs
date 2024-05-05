5c7ba01080cad9864b7e7e9162ac2a6d925e31c2
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2024-05-05 21:39:37 +0200

    Rework LocalNetworkRunner to support different actions per party and use this feature to simplify ObliviousTransferTest

--- CompactMPC.Tests/ObliviousTransferTest.cs
+++ CompactMPC.Tests/ObliviousTransferTest.cs
@@ -11,49 +11,47 @@ namespace CompactMPC
     [TestClass]
     public class ObliviousTransferTest
     {
+        private static readonly Quadruple<Message>[] Options = [
+            new(
+                CreateMessage("Zebra"),
+                CreateMessage("Mouse"),
+                CreateMessage("Whale"),
+                CreateMessage("Sheep")
+            ),
+            new(
+                CreateMessage("China"),
+                CreateMessage("India"),
+                CreateMessage("Japan"),
+                CreateMessage("Nepal")
+            ),
+            new(
+                CreateMessage("Apple"),
+                CreateMessage("Pizza"),
+                CreateMessage("Melon"),
+                CreateMessage("Bread")
+            )
+        ];
+
         [TestMethod]
         public Task TestNaorPinkasObliviousTransfer()
         {
-            return LocalNetworkRunner.RunTwoPartyNetwork(PerformObliviousTransfer);
+            return LocalNetworkRunner.RunTwoPartyNetwork(PerformSenderAsync, PerformReceiverAsync);
         }
 
-        private static async Task PerformObliviousTransfer(ITwoPartyNetworkSession session)
+        private static async Task PerformSenderAsync(ITwoPartyNetworkSession session)
         {
-            Quadruple<Message>[] options =
-            [
-                new(
-                    CreateMessage("Zebra"),
-                    CreateMessage("Mouse"),
-                    CreateMessage("Whale"),
-                    CreateMessage("Sheep")
-                ),
-                new(
-                    CreateMessage("China"),
-                    CreateMessage("India"),
-                    CreateMessage("Japan"),
-                    CreateMessage("Nepal")
-                ),
-                new(
-                    CreateMessage("Apple"),
-                    CreateMessage("Pizza"),
-                    CreateMessage("Melon"),
-                    CreateMessage("Bread")
-                )
-            ];
+            var obliviousTransfer = new NaorPinkasObliviousTransfer(SecurityParameters.CreateDefault768Bit());
+            await obliviousTransfer.SendAsync(session.Channel, Options, 3, 5);
+        }
 
+        private static async Task PerformReceiverAsync(ITwoPartyNetworkSession session)
+        {
             var obliviousTransfer = new NaorPinkasObliviousTransfer(SecurityParameters.CreateDefault768Bit());
 
-            if (session.LocalParty.Id == 0)
-            {
-                await obliviousTransfer.SendAsync(session.Channel, options, 3, 5);
-            }
-            else
-            {
-                var indices = new QuadrupleIndexArray([0, 3, 2]);
-                var results = await obliviousTransfer.ReceiveAsync(session.Channel, indices, 3, 5);
+            var indices = new QuadrupleIndexArray([0, 3, 2]);
+            var results = await obliviousTransfer.ReceiveAsync(session.Channel, indices, 3, 5);
 
-                results.Should().Equal([options[0][0], options[1][3], options[2][2]]);
-            }
+            results.Should().Equal([Options[0][0], Options[1][3], Options[2][2]]);
         }
 
         private static Message CreateMessage(string messageText) => new(Encoding.ASCII.GetBytes(messageText));
--- CompactMPC/Networking/LocalNetworkRunner.cs
+++ CompactMPC/Networking/LocalNetworkRunner.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -8,48 +7,48 @@ namespace CompactMPC.Networking
 {
     public static class LocalNetworkRunner
     {
-        private const int DefaultStartPort = 16741;
+        private const int StartPort = 16741;
 
-        public static async Task RunMultiPartyNetwork(int numberOfParties, Func<IMultiPartyNetworkSession, Task> perPartyAction, int startPort = DefaultStartPort)
+        public static Task RunMultiPartyNetwork(int numberOfParties, Func<IMultiPartyNetworkSession, Task> eachPartyAction)
         {
-            IPEndPoint[] endPoints = Enumerable
-                .Range(0, numberOfParties)
-                .Select(partyId => new IPEndPoint(IPAddress.Loopback, startPort + partyId))
-                .ToArray();
-
-            IEnumerable<Task<TcpMultiPartyNetworkSession>> sessionTasks = Enumerable
-                .Range(0, numberOfParties)
-                .Select(partyId => TcpMultiPartyNetworkSession.EstablishAsync(new Party(partyId), endPoints[partyId], endPoints.Without(endPoints[partyId]).ToArray()));
-            
-            await RunAndDispose(sessionTasks, perPartyAction);
+            return RunMultiPartyNetwork(Enumerable.Repeat(eachPartyAction, numberOfParties).ToArray());
         }
         
-        public static async Task RunTwoPartyNetwork(Func<ITwoPartyNetworkSession, Task> perPartyAction, int startPort = DefaultStartPort)
+        public static Task RunMultiPartyNetwork(params Func<IMultiPartyNetworkSession, Task>[] partyActions)
         {
-            Party firstParty = new Party(0);
-            Party secondParty = new Party(1);
+            var endPoints = partyActions
+                .Select((_, index) => new IPEndPoint(IPAddress.Loopback, StartPort + index))
+                .ToArray();
 
-            IPEndPoint firstEndPoint = new IPEndPoint(IPAddress.Loopback, startPort);
-            IPEndPoint secondEndPoint = new IPEndPoint(IPAddress.Loopback, startPort + 1);
-            
-            Task<TcpTwoPartyNetworkSession>[] sessionTasks =
-            {
-                TcpTwoPartyNetworkSession.EstablishAsync(firstParty, firstEndPoint, secondEndPoint),
-                TcpTwoPartyNetworkSession.EstablishAsync(secondParty, secondEndPoint, firstEndPoint)
-            };
+            var sessionTasks = partyActions.Select(
+                (partyAction, index) => EstablishMultiPartyAsync(index, endPoints).AndThenAsync(partyAction)
+            );
 
-            await RunAndDispose(sessionTasks, perPartyAction);
+            return Task.WhenAll(sessionTasks);
         }
 
-        private static async Task RunAndDispose<T>(IEnumerable<Task<T>> sessionTasks, Func<T, Task> perPartyAction)
+        private static Task<TcpMultiPartyNetworkSession> EstablishMultiPartyAsync(int index, IPEndPoint[] endPoints) =>
+            TcpMultiPartyNetworkSession.EstablishAsync(new Party(index), endPoints[index], endPoints.Without(endPoints[index]).ToArray());
+
+        public static Task RunTwoPartyNetwork(Func<ITwoPartyNetworkSession, Task> firstPartyAction, Func<ITwoPartyNetworkSession, Task> secondPartyAction)
+        {
+            var firstParty = new Party(0);
+            var secondParty = new Party(1);
+
+            var firstEndPoint = new IPEndPoint(IPAddress.Loopback, StartPort);
+            var secondEndPoint = new IPEndPoint(IPAddress.Loopback, StartPort + 1);
+
+            return Task.WhenAll(
+                TcpTwoPartyNetworkSession.EstablishAsync(firstParty, firstEndPoint, secondEndPoint).AndThenAsync(firstPartyAction),
+                TcpTwoPartyNetworkSession.EstablishAsync(secondParty, secondEndPoint, firstEndPoint).AndThenAsync(secondPartyAction)
+            );
+        }
+
+        private static async Task AndThenAsync<T>(this Task<T> sessionTask, Func<T, Task> sessionAction)
             where T : IDisposable
         {
-            T[] sessions = await Task.WhenAll(sessionTasks);
-            await Task.WhenAll(sessions.Select(perPartyAction));
-            foreach (T session in sessions)
-            {
-                session.Dispose();
-            }
+            using var session = await sessionTask;
+            await sessionAction(session);
         }
     }
 }
