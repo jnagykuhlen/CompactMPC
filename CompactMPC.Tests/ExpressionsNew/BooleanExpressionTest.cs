0431108037188cd410265b460e67c0409d50412f
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2026-07-08 19:41:29 +0200

    Remove bit converter classes and update unit tests

---
+++ CompactMPC.Tests/ExpressionsNew/BooleanExpressionTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompactMPC.ExpressionsNew;
+
+[TestClass]
+public class BooleanExpressionTest
+{
+    [TestMethod]
+    public void TestWriteBits()
+    {
+        var expression = BooleanExpression.False;
+        var bits = BitArray.FromBinaryString("011");
+
+        expression.WriteBits(false, bits, 1);
+        bits.ToBinaryString().Should().Be("001");
+
+        expression.WriteBits(true, bits, 0);
+        bits.ToBinaryString().Should().Be("101");
+    }
+
+    [TestMethod]
+    public void TestReadValue()
+    {
+        var expression = BooleanExpression.False;
+        var bits = BitArray.FromBinaryString("011");
+
+        expression.ReadValue(bits, 0).Should().Be(false);
+        expression.ReadValue(bits, 1).Should().Be(true);
+    }
+}
---
+++ CompactMPC.Tests/ExpressionsNew/IntegerExpressionTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompactMPC.ExpressionsNew;
+
+[TestClass]
+public class IntegerExpressionTest
+{
+    [TestMethod]
+    public void TestWriteBits()
+    {
+        var expression = IntegerExpression.AssignableUpTo(15);
+
+        var bits = BitArray.FromBinaryString("011011");
+
+        expression.WriteBits(10, bits, 2);
+        bits.ToBinaryString().Should().Be("010101");
+
+        expression.WriteBits(3, bits, 0);
+        bits.ToBinaryString().Should().Be("110001");
+    }
+    
+    [TestMethod]
+    public void TestWriteBitsOverflow()
+    {
+        var expression = IntegerExpression.AssignableUpTo(15);
+
+        var bits = BitArray.FromBinaryString("011011");
+
+        var writeAction = () => expression.WriteBits(17, bits, 0);
+
+        writeAction.Should().Throw<ArgumentException>();
+    }
+    
+    [TestMethod]
+    public void TestWriteOutOfBounds()
+    {
+        var expression = IntegerExpression.AssignableUpTo(15);
+
+        var bits = BitArray.FromBinaryString("011011");
+
+        var writeAction = () => expression.WriteBits(3, bits, 4);
+
+        writeAction.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void TestReadValue()
+    {
+        var expression = IntegerExpression.AssignableUpTo(15);
+        var bits = BitArray.FromBinaryString("011010");
+
+        expression.ReadValue(bits, 0).Should().Be(6);
+        expression.ReadValue(bits, 1).Should().Be(11);
+        expression.ReadValue(bits, 2).Should().Be(5);
+    }
+}
