889f4942557076cad3fc70bf714b96dad8ddd77e
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2026-07-09 18:01:03 +0200

    Reformat RandomNumberGenerator

--- CompactMPC/Cryptography/RandomNumberGenerator.cs
+++ CompactMPC/Cryptography/RandomNumberGenerator.cs
@@ -1,24 +1,21 @@
 ﻿using System.Numerics;
 
-namespace CompactMPC.Cryptography
+namespace CompactMPC.Cryptography;
+
+public static class RandomNumberGenerator
 {
-    public static class RandomNumberGenerator
+    public static byte[] GetBytes(int numberOfBytes) =>
+        System.Security.Cryptography.RandomNumberGenerator.GetBytes(numberOfBytes);
+
+    public static BitArray GetBits(int numberOfBits)
     {
-        public static byte[] GetBytes(int numberOfBytes)
-        {
-            return System.Security.Cryptography.RandomNumberGenerator.GetBytes(numberOfBytes);
-        }
+        var randomBytes = GetBytes(BitArray.RequiredBytes(numberOfBits));
+        return BitArray.FromBytes(randomBytes, numberOfBits);
+    }
 
-        public static BitArray GetBits(int numberOfBits)
-        {
-            byte[] randomBytes = GetBytes(BitArray.RequiredBytes(numberOfBits));
-            return BitArray.FromBytes(randomBytes, numberOfBits);
-        }
-
-        public static BigInteger GetBigInteger(int sizeInBytes)
-        {
-            byte[] randomBytes = GetBytes(sizeInBytes);
-            return new BigInteger(randomBytes, true);
-        }
+    public static BigInteger GetBigInteger(int sizeInBytes)
+    {
+        var randomBytes = GetBytes(sizeInBytes);
+        return new BigInteger(randomBytes, true);
     }
 }
