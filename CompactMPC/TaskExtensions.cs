23cadb48efa0d9a3e03b41a366ec21717ee1890b
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2026-07-21 18:11:20 +0200

    Remove unused extension methods

--- CompactMPC/TaskExtensions.cs
+++ CompactMPC/TaskExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,27 +6,6 @@ namespace CompactMPC;
 
 public static class TaskExtensions
 {
-    public static Task<T>[] ToSubTasks<T>(this Task<T[]> superTask, int numberOfSubTasks)
-    {
-        var subTasks = new Task<T>[numberOfSubTasks];
-        for (var i = 0; i < numberOfSubTasks; ++i)
-        {
-            var index = i;
-            subTasks[i] = superTask.ContinueWith(task => task.Result[index]);
-        }
-
-        return subTasks;
-    }
-
-    public static Task<TSub>[] ToSubTasks<TSub, TSuper>(this Task<TSuper> superTask, Func<TSuper, TSub[]> selector, int numberOfSubTasks) =>
-        superTask.ContinueWith(task => selector(task.Result)).ToSubTasks(numberOfSubTasks);
-
-    public static Task<T[]> ToSuperTask<T>(this Task<T>[] subTasks) =>
-        Task.WhenAll(subTasks);
-
-    public static Task<TSuper> ToSuperTask<TSub, TSuper>(this Task<TSub>[] subTasks, Func<TSub[], TSuper> selector) =>
-        subTasks.ToSuperTask().ContinueWith(task => selector(task.Result));
-
     public static async Task<T[]> AndThenAll<T>(this Task<T> task, IEnumerable<Task<T>> otherTasks)
     {
         await task;
