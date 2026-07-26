f748c57b0fe12921dde8a2cda8e1524c28e16113
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2026-07-26 12:20:40 +0200

    Add BitArrayExpression

---
+++ CompactMPC/Expressions/BitArrayExpression.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CompactMPC.Circuits;
+using CompactMPC.Collections;
+using CompactMPC.Protocol;
+
+namespace CompactMPC.Expressions;
+
+public class BitArrayExpression(IReadOnlyList<Wire> wires) : Expression(wires),
+    IInputExpression<IReadOnlyList<Bit>>, IOutputExpression<BitArray>
+{
+    public void WriteTo(IReadOnlyList<Bit> value, IWriteOnlyList<Bit> destination)
+    {
+        for (var i = 0; i < value.Count; ++i)
+            destination[i] = value[i];
+    }
+
+    public BitArray ReadFrom(IReadOnlyList<Bit> source) => new(source);
+
+    public static BitArrayExpression AllZeroes(int numberOfBits) =>
+        new(Enumerable.Repeat(Wire.Zero, numberOfBits).ToArray());
+
+    public static BitArrayExpression AllOnes(int numberOfBits) =>
+        new(Enumerable.Repeat(Wire.One, numberOfBits).ToArray());
+
+    public static BitArrayExpression Xor(IReadOnlyList<BitArrayExpression> values) =>
+        values.AggregateDepthEfficient((x, y) => x ^ y);
+
+    public static BitArrayExpression And(IReadOnlyList<BitArrayExpression> values) =>
+        values.AggregateDepthEfficient((x, y) => x & y);
+
+    public static BitArrayExpression operator ^(BitArrayExpression left, BitArrayExpression right) =>
+        new(left.Wires.Zip(right.Wires, Wire.Xor).ToArray());
+
+    public static BitArrayExpression operator &(BitArrayExpression left, BitArrayExpression right) =>
+        new(left.Wires.Zip(right.Wires, Wire.And).ToArray());
+
+    public static BitArrayExpression operator ~(BitArrayExpression left) =>
+        new(left.Wires.Select(Wire.Not).ToArray());
+
+    public static Input<BitArrayExpression> Input(int numberOfBits) => new(() => Assignable(numberOfBits));
+    public static Output<BitArrayExpression> Output() => new();
+
+    private static BitArrayExpression Assignable(int numberOfBits) =>
+        new(
+            Enumerable
+                .Range(0, numberOfBits)
+                .Select(_ => Wire.Assignable())
+                .ToArray()
+        );
+}
